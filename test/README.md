d3f6de977363f1df256c60bdcbdc907fdfa2aad2
Naol <nlme9321@gmail.com>
2026-08-24 01:50:53 +0300

    Added test cases

--- test/README.md
+++
@@ -1,3 +0,0 @@
-# Mobile Unit & Widget Tests
-
-Store Flutter widget tests and repository unit tests here.
---
+++ test/widget_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:revola_app/core/providers/core_providers.dart';
+import 'package:revola_app/core/storage/storage_service.dart';
+import 'package:revola_app/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  testWidgets('RevolaApp smoke test', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+    final storageService = StorageService();
+    await storageService.init();
+
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [storageServiceProvider.overrideWithValue(storageService)],
+        child: const RevolaApp(),
+      ),
+    );
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+    expect(find.byType(RevolaApp), findsOneWidget);
+  });
+}
